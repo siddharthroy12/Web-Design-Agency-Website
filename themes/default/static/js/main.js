06bbf7fd5efdb9f4a86751432d6ebccb99a43910
Siddharth Roy <siddharthroy36912@gmail.com>
2021-04-20 14:08:08 +0530

    Added animation to nav menu

--- themes/default/static/js/main.js
+++ themes/default/static/js/main.js
@@ -3,11 +3,12 @@ menuOpenBtn = document.getElementById('menu-open-btn')
 menuCloseBtn = document.getElementById('menu-close-btn')
 
 function hideMenu() {
-	menuNav.style.display = 'none'
+	menuNav.style.maxWidth = '0rem'
 }
 
 function showMenu() {
-	menuNav.style.display = 'block'
+	menuNav.style.maxWidth = '18rem'
+	console.log('s')
 }
 
 menuOpenBtn.addEventListener('click', showMenu)
